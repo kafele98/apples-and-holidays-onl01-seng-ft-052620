75a0d8c54bfb068bea8a06405beaa5b5e4b9a767
Zoe Keller <keller.zoe98@gmail.com>
2020-06-10 01:52:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ def all_supplies_in_holidays(holiday_hash)
   end 
 end
 
-all_holidays_with_bbq(:holiday_supplies)
+all_supplies_in_holidays(:holiday_supplies)
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
